000110bc478532dad6181bb33d3cb0f400502bca
Matt Ward <matt.ward@microsoft.com>
2020-01-08 11:48:23 +0000

    Add simple console app for the NuGet credential token cache

---
+++ NuGetCredentialProviderSessionTokenCache/Program.cs
@@ -0,0 +1,101 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2020 Microsoft Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Threading;
+using NuGetCredentialProvider.Logging;
+using NuGetCredentialProvider.Util;
+
+namespace NuGetCredentialProviderSessionTokenCache
+{
+	class MainClass
+	{
+		static readonly string LocalAppDataLocation = Path.Combine (
+			Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create),
+			"MicrosoftCredentialProvider");
+
+		static string SessionTokenCacheLocation { get; } = Path.Combine (LocalAppDataLocation, "SessionTokenCache.dat");
+
+		public static void Main (string[] args)
+		{
+			try {
+				if (args.Length > 0) {
+					if (args [0] == "add") {
+						if (args.Length == 3) {
+							AddToCache (args [1], args [2]);
+						}
+					} else if (args [0] == "list") {
+						ListKeys ();
+					}
+					return;
+				}
+
+				PrintUsage ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex);
+			}
+		}
+
+		static void PrintUsage ()
+		{
+			Console.WriteLine ("Usage:");
+			Console.WriteLine ("    add uri token");
+			Console.WriteLine ("    list");
+		}
+
+		static void AddToCache (string uri, string token)
+		{
+			var cache = new SessionTokenCache (
+				SessionTokenCacheLocation,
+				new ConsoleLogger (),
+				CancellationToken.None);
+
+			cache [new Uri (uri)] = token;
+		}
+
+		static void ListKeys ()
+		{
+			if (File.Exists (SessionTokenCacheLocation)) {
+				Console.WriteLine ("SessionTokenCache found at {0}", SessionTokenCacheLocation);
+			} else {
+				Console.WriteLine ("SessionTokenCache not found at {0}", SessionTokenCacheLocation);
+				return;
+			}
+
+			Console.WriteLine ();
+			Console.WriteLine ("Uris:");
+			var cache = new SessionTokenCache (
+				SessionTokenCacheLocation,
+				new ConsoleLogger (),
+				CancellationToken.None);
+
+			foreach (Uri key in cache.Cache.Keys) {
+				Console.WriteLine (key);
+			}
+		}
+	}
+}
---
+++ NuGetCredentialProviderSessionTokenCache/SessionTokenCache.cs
@@ -0,0 +1,189 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using NuGetCredentialProvider.Logging;
+using NuGetCredentialProviderSessionTokenCache;
+
+namespace NuGetCredentialProvider.Util
+{
+    public class SessionTokenCache : ICache<Uri, string>
+    {
+        private static readonly object FileLock = new object ();
+        private readonly string cacheFilePath;
+        private ILogger logger;
+        private CancellationToken cancellationToken;
+        private readonly string mutexName;
+
+        public SessionTokenCache (string cacheFilePath, ILogger logger, CancellationToken cancellationToken)
+        {
+            this.cacheFilePath = cacheFilePath;
+            this.logger = logger;
+            this.mutexName = @"Global\" + cacheFilePath.Replace (Path.DirectorySeparatorChar, '_');
+        }
+
+        public Dictionary<Uri, string> Cache {
+            get {
+                bool mutexHeld = false, dummy;
+                using (Mutex mutex = new Mutex (false, mutexName, out dummy)) {
+                    try {
+                        try {
+                            if (!mutex.WaitOne (0)) {
+                                // We couldn't get the mutex on our first acquisition attempt. Log this so the user knows what we're
+                                // waiting on.
+                                logger.Verbose (Resources.SessionTokenCacheMutexMiss);
+
+                                int index = WaitHandle.WaitAny (new WaitHandle[] { mutex, this.cancellationToken.WaitHandle }, -1);
+
+                                if (index == 1) {
+                                    logger.Verbose (Resources.CancelMessage);
+                                    return new Dictionary<Uri, string> ();
+                                } else if (index == WaitHandle.WaitTimeout) {
+                                    logger.Verbose (Resources.SessionTokenCacheMutexFail);
+                                    return new Dictionary<Uri, string> ();
+                                }
+                            }
+                        } catch (AbandonedMutexException) {
+                            // If this is thrown, then we hold the mutex.
+                        }
+
+                        mutexHeld = true;
+
+                        return Deserialize (ReadFileBytes ());
+                    } finally {
+                        if (mutexHeld) {
+                            mutex.ReleaseMutex ();
+                        }
+                    }
+                }
+            }
+        }
+
+        public string this[Uri key] {
+            get => Cache[key];
+            set {
+                bool mutexHeld = false, dummy;
+                using (Mutex mutex = new Mutex (false, mutexName, out dummy)) {
+                    try {
+                        try {
+                            if (!mutex.WaitOne (0)) {
+                                // We couldn't get the mutex on our first acquisition attempt. Log this so the user knows what we're
+                                // waiting on.
+                                logger.Verbose (Resources.SessionTokenCacheMutexMiss);
+
+                                int index = WaitHandle.WaitAny (new WaitHandle[] { mutex, this.cancellationToken.WaitHandle }, -1);
+
+                                if (index == 1) {
+                                    logger.Verbose (Resources.CancelMessage);
+                                } else if (index == WaitHandle.WaitTimeout) {
+                                    logger.Verbose (Resources.SessionTokenCacheMutexFail);
+                                }
+                            }
+                        } catch (AbandonedMutexException) {
+                            // If this is thrown, then we hold the mutex.
+                        }
+
+                        mutexHeld = true;
+
+                        var cache = Cache;
+                        cache[key] = value;
+                        WriteFileBytes (Serialize (cache));
+                    } finally {
+                        if (mutexHeld) {
+                            mutex.ReleaseMutex ();
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool ContainsKey (Uri key)
+        {
+            return Cache.ContainsKey (key);
+        }
+
+        public bool TryGetValue (Uri key, out string value)
+        {
+            try {
+                return Cache.TryGetValue (key, out value);
+            } catch (Exception e) {
+                if (File.Exists (cacheFilePath)) {
+                    File.Delete (cacheFilePath);
+                }
+
+                logger.Verbose (string.Format (Resources.CacheException, e.Message));
+                Cache.Clear ();
+                value = null;
+
+                return false;
+            }
+        }
+
+        public void Remove (Uri key)
+        {
+            bool mutexHeld = false, dummy;
+            using (Mutex mutex = new Mutex (false, mutexName, out dummy)) {
+                try {
+                    try {
+                        if (!mutex.WaitOne (0)) {
+                            // We couldn't get the mutex on our first acquisition attempt. Log this so the user knows what we're
+                            // waiting on.
+                            logger.Verbose (Resources.SessionTokenCacheMutexMiss);
+
+                            int index = WaitHandle.WaitAny (new WaitHandle[] { mutex, this.cancellationToken.WaitHandle }, -1);
+
+                            if (index == 1) {
+                                logger.Verbose (Resources.CancelMessage);
+                            } else if (index == WaitHandle.WaitTimeout) {
+                                logger.Verbose (Resources.SessionTokenCacheMutexFail);
+                            }
+                        }
+                    } catch (AbandonedMutexException) {
+                        // If this is thrown, then we hold the mutex.
+                    }
+
+                    mutexHeld = true;
+
+                    var cache = Cache;
+                    cache.Remove (key);
+                    WriteFileBytes (Serialize (cache));
+                } finally {
+                    if (mutexHeld) {
+                        mutex.ReleaseMutex ();
+                    }
+                }
+            }
+        }
+
+        private Dictionary<Uri, string> Deserialize (byte[] data)
+        {
+            if (data == null) {
+                return new Dictionary<Uri, string> ();
+            }
+
+            var serialized = System.Text.Encoding.UTF8.GetString (data);
+            return JsonConvert.DeserializeObject<Dictionary<Uri, string>> (serialized);
+        }
+
+        private byte[] Serialize (Dictionary<Uri, string> data)
+        {
+            var serialized = JsonConvert.SerializeObject (data);
+            return System.Text.Encoding.UTF8.GetBytes (serialized);
+        }
+
+        private byte[] ReadFileBytes ()
+        {
+            return EncryptedFile.ReadFileBytes (cacheFilePath, readUnencrypted: true);
+        }
+
+        private void WriteFileBytes (byte[] bytes)
+        {
+            EncryptedFile.WriteFileBytes (cacheFilePath, bytes, writeUnencrypted: true);
+        }
+    }
+}
